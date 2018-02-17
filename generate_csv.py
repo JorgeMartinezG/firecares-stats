01744ce1798ad0a1f1e65c76cbafbab09e85deb5
Jorge Martinez <jorgemg0327@gmail.com>
2018-02-17 10:12:20 -0500

    Included resolved_at, closed_at and first_responded_at fields

--- generate_csv.py
+++ generate_csv.py
@@ -16,6 +16,13 @@ def create_fd_groups(api_key):
     return fd_groups
 
 
+def include_stats_fields(ticket):
+    for key, value in ticket['stats'].items():
+        ticket[key] = value
+
+    return ticket
+
+
 def save_response(response, page_no, folder_name='jsons'):
     # Create folder if does not exist.
     if not os.path.isdir(folder_name):
@@ -32,11 +39,14 @@ def get_firecares_response(page_number, query_string, api_key):
 
     # Perform request to given url.
     response = requests.get(url=endpoint + urlencode(query_string),
-                            auth=(api_key, 'X'))
+                            auth=(api_key, 'X')).json()
+
+    # Including stats into one higher level in json tree.
+    response = [include_stats_fields(ticket) for ticket in response]
 
     # Save response into json file.
     #save_response(response.json(), page_number)
-    return response.json()
+    return response
 
 
 def Main():
@@ -47,7 +57,8 @@ def Main():
     query_string =dict(updated_since='2017-01-01T00:00:00Z',
                        order_by='created_at',
                        order_type='desc',
-                       per_page='100'
+                       per_page='100',
+                       include='stats'
     )
     # Firecares url
     fd_groups = create_fd_groups(api_key)
@@ -71,7 +82,8 @@ def Main():
     df['assigned_group'] = df['group_id'].apply(lambda x: fd_groups.get(x,
                                                           'Unassigned'))
 
-    cols = ["id", "subject", "status_name", "assigned_group", "created_at", "updated_at"]
+    cols = ["id", "subject", "status_name", "assigned_group", "created_at", "updated_at",
+            "first_responded_at", "closed_at", "resolved_at"]
     terse_df = df[cols]
     terse_df.to_csv('tickets.csv')
 
